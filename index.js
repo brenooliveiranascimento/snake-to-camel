cf406ffd41ae723c68586ac7e5330a78833dae36
Breno de Oliveira Nascimento <94801880+brenooliveiranascimento@users.noreply.github.com>
2024-07-12 18:59:17 -0300

    subindo projeto

---
+++ index.js
@@ -0,0 +1,29 @@
+const formatKey = (snakeStr) => {
+  return snakeStr.toLowerCase().replace(/(_\w)/g, (match) => {
+    return match[1].toUpperCase();
+  });
+};
+
+const convert = (items) =>
+  Object.entries(items).reduce((acc, [key, value]) => {
+    return {
+      ...acc,
+      [formatKey(key)]:
+        typeof value === "object" && value?.length
+          ? value.map(convert)
+          : typeof value === "object"
+          ? convert(value)
+          : value,
+    };
+  }, {});
+
+export const snakeToCamel = (items) => {
+  if (typeof items !== "object") {
+    throw new Error("The parameter must be an object or an array!");
+  }
+  if (items?.length) {
+    return items.map(convert);
+  } else {
+    return convert(items);
+  }
+};
