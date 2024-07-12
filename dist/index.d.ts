5b82ab734872488aad1afd8969b310e522f8ee05
Breno de Oliveira Nascimento <94801880+brenooliveiranascimento@users.noreply.github.com>
2024-07-12 20:52:40 -0300

    alterando métodos

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,5 @@
-export declare const formatKey: (snakeStr: string) => string;
-export declare const convert: any;
-export declare const snakeToCamel: (items: object) => any;
+export declare const formatKeyToCamel: (snakeStr: string) => string;
+export declare const formateKeyToSnake: (camelStr: string) => string;
+export declare const convert: (items: object, converter: (param: string) => string) => object;
+export declare const snakeToCamel: (items: object) => object | object[];
+export declare const camelToSnake: (items: object) => object | object[];
--- dist/index.js
+++ dist/index.js
@@ -11,32 +11,49 @@ var __assign = (this && this.__assign) || function () {
     return __assign.apply(this, arguments);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.snakeToCamel = exports.convert = exports.formatKey = void 0;
-exports.formatKey = function (snakeStr) {
+exports.camelToSnake = exports.snakeToCamel = exports.convert = exports.formateKeyToSnake = exports.formatKeyToCamel = void 0;
+exports.formatKeyToCamel = function (snakeStr) {
     return snakeStr.toLowerCase().replace(/(_\w)/g, function (match) {
         return match[1].toUpperCase();
     });
 };
-exports.convert = function (items) {
+exports.formateKeyToSnake = function (camelStr) {
+    return camelStr.replace(/[A-Z]/g, function (match) { return "_" + match.toLowerCase(); });
+};
+exports.convert = function (items, converter) {
     return Object.entries(items).reduce(function (acc, _a) {
         var _b;
         var key = _a[0], value = _a[1];
-        return __assign(__assign({}, acc), (_b = {}, _b[exports.formatKey(key)] = Array.isArray(value)
-            ? value.map(exports.convert)
+        return __assign(__assign({}, acc), (_b = {}, _b[converter(key)] = Array.isArray(value)
+            ? value.map(function (currValue) {
+                return typeof currValue === "object"
+                    ? exports.convert(currValue, converter)
+                    : currValue;
+            })
             : typeof value === "object"
-                ? exports.convert(value)
+                ? exports.convert(value, converter)
                 : value, _b));
     }, {});
 };
 exports.snakeToCamel = function (items) {
     if (typeof items !== "object") {
-        throw new Error("The parameter must be an object or an array!");
+        throw new Error("The parameter must be an object or an array of objects!");
     }
     if (Array.isArray(items)) {
-        return items.map(exports.convert);
+        return items.map(function (item) { return exports.convert(item, exports.formatKeyToCamel); });
     }
     else {
-        return exports.convert(items);
+        return exports.convert(items, exports.formatKeyToCamel);
     }
 };
-console.log();
+exports.camelToSnake = function (items) {
+    if (typeof items !== "object") {
+        throw new Error("The parameter must be an object or an array of objects!");
+    }
+    if (Array.isArray(items)) {
+        return items.map(function (item) { return exports.convert(item, exports.formateKeyToSnake); });
+    }
+    else {
+        return exports.convert(items, exports.formateKeyToSnake);
+    }
+};
