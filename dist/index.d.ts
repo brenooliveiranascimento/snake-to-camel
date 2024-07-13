073d716a08a19c338c0079b5074ef1d92d540e83
Breno de Oliveira Nascimento <94801880+brenooliveiranascimento@users.noreply.github.com>
2024-07-13 09:27:12 -0300

    gerando build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 export declare const formatKeyToCamel: (snakeStr: string) => string;
 export declare const formateKeyToSnake: (camelStr: string) => string;
-export declare const snakeToCamel: (items: object | object[]) => object | object[];
-export declare const camelToSnake: (items: object | object[]) => object | object[];
+export declare const snakeToCamel: (items: object | object[]) => any | any[];
+export declare const camelToSnake: (items: object | object[]) => any | any[];
