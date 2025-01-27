c59c1b0d62984eeaece37a930ad716d386f45988
Breno Nascimento <satsukebare23@gmail.com>
2025-01-27 13:04:06 -0300

    Adicionando compatibilidade com datas

--- dist/index.js
+++ dist/index.js
@@ -1,59 +1,60 @@
 "use strict";
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.camelToSnake = exports.snakeToCamel = exports.formateKeyToSnake = exports.formatKeyToCamel = void 0;
-exports.formatKeyToCamel = function (snakeStr) {
+const formatKeyToCamel = (snakeStr) => {
     if (!snakeStr.includes("_")) {
         return snakeStr;
     }
-    return snakeStr.toLowerCase().replace(/(_\w)/g, function (match) {
+    return snakeStr.toLowerCase().replace(/(_\w)/g, (match) => {
         return match[1].toUpperCase();
     });
 };
-exports.formateKeyToSnake = function (camelStr) {
-    return camelStr.replace(/[A-Z]/g, function (match) { return "_" + match.toLowerCase(); });
+exports.formatKeyToCamel = formatKeyToCamel;
+const formateKeyToSnake = (camelStr) => {
+    return camelStr.replace(/[A-Z]/g, (match) => "_" + match.toLowerCase());
 };
-var convertObject = function (originalObject, converter) {
-    return Object.entries(originalObject).reduce(function (acc, _a) {
-        var _b;
-        var key = _a[0], value = _a[1];
-        return __assign(__assign({}, acc), (_b = {}, _b[converter(key)] = !value
-            ? value
-            : Array.isArray(value)
-                ? value.map(function (currValue) {
-                    return typeof currValue === "object"
-                        ? convert(currValue, converter)
-                        : currValue;
-                })
-                : typeof value === "object"
-                    ? convert(value, converter)
-                    : value, _b));
+exports.formateKeyToSnake = formateKeyToSnake;
+const isDate = (value) => {
+    return value instanceof Date && !isNaN(value.getTime());
+};
+const convertObject = (originalObject, converter) => {
+    return Object.entries(originalObject).reduce((acc, [key, value]) => {
+        let convertedValue = value;
+        if (value) {
+            if (isDate(value)) {
+                convertedValue = value;
+            }
+            else if (Array.isArray(value)) {
+                convertedValue = value.map((currValue) => typeof currValue === "object" && !isDate(currValue)
+                    ? convert(currValue, converter)
+                    : currValue);
+            }
+            else if (typeof value === "object" && !isDate(value)) {
+                convertedValue = convert(value, converter);
+            }
+        }
+        return {
+            ...acc,
+            [converter(key)]: convertedValue,
+        };
     }, {});
 };
-var convert = function (items, converter) {
+const convert = (items, converter) => {
     if (Array.isArray(items)) {
-        return items.map(function (item) { return convertObject(item, converter); });
+        return items.map((item) => convertObject(item, converter));
     }
-    else if (typeof items === "object") {
+    else if (typeof items === "object" && !isDate(items)) {
         return convertObject(items, converter);
     }
     else {
         throw new Error("Parameter must be an object or an array of objects!");
     }
 };
-exports.snakeToCamel = function (items) {
+const snakeToCamel = (items) => {
     return convert(items, exports.formatKeyToCamel);
 };
-exports.camelToSnake = function (items) {
+exports.snakeToCamel = snakeToCamel;
+const camelToSnake = (items) => {
     return convert(items, exports.formateKeyToSnake);
 };
+exports.camelToSnake = camelToSnake;
--- src/index.ts
+++ src/index.ts
@@ -11,24 +11,34 @@ export const formateKeyToSnake = (camelStr: string): string => {
   return camelStr.replace(/[A-Z]/g, (match) => "_" + match.toLowerCase());
 };
 
+const isDate = (value: any): boolean => {
+  return value instanceof Date && !isNaN(value.getTime());
+};
+
 const convertObject = (
   originalObject: object,
   converter: (param: string) => string
 ): object => {
   return Object.entries(originalObject).reduce((acc, [key, value]) => {
+    let convertedValue = value;
+
+    if (value) {
+      if (isDate(value)) {
+        convertedValue = value;
+      } else if (Array.isArray(value)) {
+        convertedValue = value.map((currValue) =>
+          typeof currValue === "object" && !isDate(currValue)
+            ? convert(currValue, converter)
+            : currValue
+        );
+      } else if (typeof value === "object" && !isDate(value)) {
+        convertedValue = convert(value, converter);
+      }
+    }
+
     return {
       ...acc,
-      [converter(key)]: !value
-        ? value
-        : Array.isArray(value)
-        ? value.map((currValue) =>
-            typeof currValue === "object"
-              ? convert(currValue, converter)
-              : currValue
-          )
-        : typeof value === "object"
-        ? convert(value, converter)
-        : value,
+      [converter(key)]: convertedValue,
     };
   }, {});
 };
@@ -39,7 +49,7 @@ const convert = (
 ): object | object[] => {
   if (Array.isArray(items)) {
     return items.map((item) => convertObject(item, converter));
-  } else if (typeof items === "object") {
+  } else if (typeof items === "object" && !isDate(items)) {
     return convertObject(items, converter);
   } else {
     throw new Error("Parameter must be an object or an array of objects!");
